798d1efbf93d1222b645b259efa4718aca77f072
Juan Manuel de las Heras Arroyo <j.heras13@gmail.com>
2023-02-17 00:38:30 +0000

    Add game logic in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,13 +9,56 @@ let playerScore=0;
 let resultMessage=""
 let playerChoice=""
 
+// Dictionary with the mapping for the buttons "data" attribute , 
+// each key corresponds  to a button and the value is an array where:
+//      index=0 --> Weapon name,
+//      index=1 --> Weapon graphic file
+//      index=2 --> Weapon value to be used as index in the outcome table
 const  weaponChoices = {
-    rock:     ["Rock"    , "jmh-rock.png"],
-    paper:    [ "Paper"  , "jmh-paper.png" ],
-    scissors: ["Scissors", "jmh-scissors.png"] ,
-    lizard:   ["Lizard"  , "jmh-lizar.png"],
-    spock:    ["Spock"   , "jmh-spock.png"]
+// Weapon Key:    [ weapon Name , Graphic file ,  Weapon number ]
+        rock:     ["Rock"       , "jmh-rock.png",     0 ],
+        paper:    ["Paper"      , "jmh-paper.png",    1 ],
+        scissors: ["Scissors"   , "jmh-scissors.png", 2 ] ,
+        lizard:   ["Lizard"     , "jmh-lizard.png",   4 ],
+        spock:    ["Spock"      , "jmh-spock.png",    3 ]
 };
+
+// The outcomeTable stores the information for the outcome of each draw.
+// The table is a 3 dimensions array  where:
+//    1st dimension index = player's choice (obtained from WeaponsChoice dictionary)
+//    2nd dimension index = computer's choice     ""
+//    3rd dimension provides the outcome and explanation values
+//      index=0  
+//          value=-1 --> Player loses, 
+//          value= 0 --> Draw, 
+//          value= 1 --> Player wins
+//          value+1 can be used as index of "outcomeMessages" to get result string.
+//      index=1
+//          value that can be used as index of "outcomeReasons" to  get the
+//          explanation text for the outcome.
+const outcomeTable = [
+    // Rock        Paper    Scissors     Spock     Lizard     // Player column 
+    [[ 0, null], [-1,   1], [+1,   9], [-1,   8], [+1,   2]], // Rock
+    [[+1,    1], [ 0,null], [-1,   0], [+1,   7], [-1,   6]], // Paper
+    [[-1,    9], [+1,   0], [ 0,null], [-1,   4], [+1,   5]], // Scissors
+    [[+1,    8], [-1,   7], [+1,   4], [ 0,null], [+1,   3]], // Spock
+    [[-1,    2], [+1,   6], [-1,   5], [+1,   3], [ 0,null]], // Lizard
+];
+const outcomeMessages = ["You lose","Draw", "You win "]
+const outcomeReasons = [
+    "Scissors cuts Paper",          //0
+    "Paper covers Rock",            //1
+    "Rock crushes Lizard",          //2
+    "Lizard poisons Spock",         //3
+    "Spock smashes Scissors",       //4
+    "Scissors decapitates Lizard",  //5
+    "Lizard eats Paper",            //6
+    "Paper disproves Spock",        //7
+    "Spock vaporizes Rock",         //8
+    "Rock crushes Scissor"          //9
+];     
+// Messages to be displayed at the end of the game.
+const finalMessages = ["Sorry!, You Lost","Wow!, You Tied", "Congratulations!, You Have Won!"]
 
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
@@ -37,7 +80,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function startGame(){
     // Reset game round counter and scores and invite the player to choose a weapon
-    round=0;
+    roundNumber=1;
     computerScore=0;
     playerScore=0;
     resultMessage="Choose a weapon";
@@ -71,10 +114,42 @@ function randomChoice() {
 
 }
 function playRound() {
-    checkOutcome();
+    let outcome=checkOutcome();
+    roundNumber++;
     displayInfo()
+    if (roundNumber > totalRounds){ // All rounds played
+        let finalOutcome=1; // Default to tie
+        if (playerScore < computerScore){         // Player lost
+            finalOutcome = 0;
+            console.log("Player lost")
+        }else if (playerScore > computerScore) {  // Player won
+            finalOutcome = 2;
+            console.log("Player won")
+        }
+        alert(finalMessages[finalOutcome]);
+    }
 }
 
 function checkOutcome() {
     console.log("checkOutcome(" +")");
+    let index1=weaponChoices[playerChoice][2];
+    let index2=weaponChoices[computerChoice][2];
+    let outcome = outcomeTable[index1][index2][0];
+    let outcomeMessage=outcomeMessages[outcome+1];
+    if (outcome === 0) {  // Player and computer have the same weapon, Draw
+        let weaponName=  weaponChoices[playerChoice][0];
+        resultMessage = outcomeMessage+", Both have choosen " + weaponName
+        }
+    else
+       {                // Player and computer have diferent weapons
+        let reason  = outcomeTable[index1][index2][1];
+        let outcomeExplain = outcomeReasons[reason];
+        resultMessage = outcomeMessage + ", " + outcomeExplain;
+        if (outcome<0) { // Player loose
+            computerScore++;
+        } else {    // player won
+            playerScore++;
+        }
+       }
+    return (outcome)
 }
